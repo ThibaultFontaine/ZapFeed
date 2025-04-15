4a1b087d3b8ff660efbe673927aeb43270498b74
clement-jny <clementjaunay@free.fr>
2025-04-16 00:55:34 +0200

    feat: add datetime on entity

--- src/Entity/UserFeed.php
+++ src/Entity/UserFeed.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\UserFeedRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Table(name: 'user_feed')]
@@ -20,8 +21,14 @@ class UserFeed
     #[ORM\JoinColumn(nullable: false)]
     private ?Feed $feed = null;
 
-    #[ORM\Column(length: 100)]
+    #[ORM\Column(type: Types::STRING, length: 100)]
     private ?string $title = null;
+
+    #[ORM\Column(name: 'created_at', type: Types::DATETIME_IMMUTABLE)]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(name: 'updated_at', type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $updatedAt = null;
 
 
     // GETTERS AND SETTERS
@@ -54,6 +61,28 @@ class UserFeed
     public function setTitle(string $title): static
     {
         $this->title = $title;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
