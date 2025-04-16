71185c6ee4e35fb0a00fda7632b8d626d4375118
clement-jny <clementjaunay@free.fr>
2025-04-16 22:14:59 +0200

    refactor: explicit get data

--- src/Controller/UserFeedController.php
+++ src/Controller/UserFeedController.php
@@ -7,8 +7,6 @@ use App\Entity\UserFeed;
 use App\Entity\Item;
 use App\Entity\UserItem;
 use App\Form\FeedType;
-use App\Repository\FeedRepository;
-use App\Repository\UserFeedRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use SimplePie\SimplePie;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,8 +17,11 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/feed')]
 final class UserFeedController extends AbstractController
 {
+    public function __construct(private EntityManagerInterface $entityManager) {}
+
+
     #[Route(name: 'app_user_feed_index', methods: ['GET'])]
-    public function index(UserFeedRepository $userFeedRepository): Response
+    public function index(): Response
     {
         $user = $this->getUser();
 
@@ -29,12 +30,12 @@ final class UserFeedController extends AbstractController
         }
 
         return $this->render('user_feed/index.html.twig', [
-            'user_feeds' => $userFeedRepository->findBy(['user' => $user]),
+            'user_feeds' => $this->entityManager->getRepository(UserFeed::class)->findBy(['user' => $user]),
         ]);
     }
 
     #[Route('/new', name: 'app_user_feed_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, FeedRepository $feedRepository): Response
+    public function new(Request $request): Response
     {
         $form = $this->createForm(FeedType::class);
         $form->handleRequest($request);
@@ -45,14 +46,14 @@ final class UserFeedController extends AbstractController
             $name = $form->get('name')->getData();
 
             // Check if a feed with this URL already exists
-            $feed = $feedRepository->findOneBy(['url' => $url]);
+            $feed = $this->entityManager->getRepository(Feed::class)->findOneBy(['url' => $url]);
 
             // If the feed does not exist, create it
             if (!$feed) {
                 $feed = new Feed();
                 $feed->setUrl($url);
-                $entityManager->persist($feed);
-                $entityManager->flush(); // Flush to save the new feed
+                $this->entityManager->persist($feed);
+                $this->entityManager->flush(); // Flush to save the new feed
             }
 
             $user = $this->getUser();
@@ -92,8 +93,8 @@ final class UserFeedController extends AbstractController
 
 
                     // Persist the feed item
-                    $entityManager->persist($item);
-                    $entityManager->flush();
+                    $this->entityManager->persist($item);
+                    $this->entityManager->flush();
 
                     $userItem = new UserItem();
                     $userItem->setUser($user);
@@ -102,13 +103,13 @@ final class UserFeedController extends AbstractController
                     $userItem->setHasBeenLiked(false);
                     $userItem->setCreatedAt(new \DateTimeImmutable());
                     $userItem->setUpdatedAt(new \DateTime());
-                    $entityManager->persist($userItem);
-                    $entityManager->flush();
+                    $this->entityManager->persist($userItem);
+                    $this->entityManager->flush();
                 }
             }
 
-            $entityManager->persist($userFeed);
-            $entityManager->flush();
+            $this->entityManager->persist($userFeed);
+            $this->entityManager->flush();
 
             return $this->redirectToRoute('app_user_feed_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -119,10 +120,14 @@ final class UserFeedController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_user_feed_show', methods: ['GET'])]
-    public function show(Feed $feed, EntityManagerInterface $entityManager): Response
+    public function show(int $id): Response
     {
         // Vérifier que l'utilisateur actuel est bien le propriétaire de ce UserFeed
-        $userFeed = $entityManager->getRepository(UserFeed::class)->findOneBy(['feed' => $feed]);
+        $feed = $this->entityManager->getRepository(Feed::class)->find($id);
+        if (!$feed) {
+            throw $this->createNotFoundException('Feed not found');
+        }
+        $userFeed = $this->entityManager->getRepository(UserFeed::class)->findOneBy(['feed' => $feed]);
         $this->checkUserFeedOwnership($userFeed);
 
         return $this->render('user_feed/show.html.twig', [
@@ -131,10 +136,14 @@ final class UserFeedController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_user_feed_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Feed $feed, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, int $id): Response
     {
         // Vérifier que l'utilisateur actuel est bien le propriétaire de ce UserFeed
-        $userFeed = $entityManager->getRepository(UserFeed::class)->findOneBy(['feed' => $feed]);
+        $feed = $this->entityManager->getRepository(Feed::class)->find($id);
+        if (!$feed) {
+            throw $this->createNotFoundException('Feed not found');
+        }
+        $userFeed = $this->entityManager->getRepository(UserFeed::class)->findOneBy(['feed' => $feed]);
         $this->checkUserFeedOwnership($userFeed);
 
         // Créer le formulaire avec les données existantes
@@ -153,7 +162,7 @@ final class UserFeedController extends AbstractController
             $userFeed->setTitle($newTitle);
             $userFeed->setUpdatedAt(new \DateTime());
 
-            $entityManager->flush();
+            $this->entityManager->flush();
 
             return $this->redirectToRoute('app_user_feed_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -165,19 +174,23 @@ final class UserFeedController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_user_feed_delete', methods: ['POST'])]
-    public function delete(Request $request, Feed $feed, EntityManagerInterface $entityManager): Response
+    public function delete(Request $request, int $id): Response
     {
         // Vérifier que l'utilisateur actuel est bien le propriétaire de ce UserFeed
-        $userFeed = $entityManager->getRepository(UserFeed::class)->findOneBy(['feed' => $feed]);
+        $feed = $this->entityManager->getRepository(Feed::class)->find($id);
+        if (!$feed) {
+            throw $this->createNotFoundException('Feed not found');
+        }
+        $userFeed = $this->entityManager->getRepository(UserFeed::class)->findOneBy(['feed' => $feed]);
         if (!$userFeed) {
             throw $this->createNotFoundException('UserFeed not found');
         }
         $this->checkUserFeedOwnership($userFeed);
 
         // if ($this->isCsrfTokenValid('delete' . $userFeed->getUser()->getId() . $userFeed->getFeed()->getId(), $request->getPayload()->getString('_token'))) {
-        if ($this->isCsrfTokenValid('delete' . $userFeed->getFeed()->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($userFeed);
-            $entityManager->flush();
+        if ($this->isCsrfTokenValid('delete' . $userFeed->getFeed()->getId(), $request->get('_token'))) {
+            $this->entityManager->remove($userFeed);
+            $this->entityManager->flush();
         }
 
         return $this->redirectToRoute('app_user_feed_index', [], Response::HTTP_SEE_OTHER);
