140fff6f956dca69ad4cc9387d4d0ed7748add86
clement-jny <clementjaunay@free.fr>
2025-04-16 22:18:38 +0200

    refactor: clean and use path fn

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -1,17 +1,16 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/about')]
 class AboutController extends AbstractController
 {
-    #[Route("/about", methods: ['GET'])]
-    public function about(): Response
+    #[Route(name: 'app_about', methods: ['GET'])]
+    public function index(): Response
     {
         return $this->render('about.html.twig');
     }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,17 +1,16 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/')]
 class HomeController extends AbstractController
 {
-    #[Route("/", methods: ['GET'])]
-    public function home(): Response
+    #[Route(name: 'app_home', methods: ['GET'])]
+    public function index(): Response
     {
         return $this->render('index.html.twig');
     }
