e259fc413791e745bda85cd6959904eb7bc2f8a1
clement-jny <clementjaunay@free.fr>
2025-04-16 10:12:05 +0200

    refactor: update index, new and edit to get and update datas

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Feed;
+use App\Entity\UserFeed;
 use App\Form\FeedType;
 use App\Repository\FeedRepository;
+use App\Repository\UserFeedRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,29 +17,67 @@ use Symfony\Component\Routing\Attribute\Route;
 final class FeedController extends AbstractController
 {
     #[Route(name: 'app_feed_index', methods: ['GET'])]
-    public function index(FeedRepository $feedRepository): Response
+    public function index(UserFeedRepository $userFeedRepository): Response
     {
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createAccessDeniedException('User not logged in');
+        }
+
+        // Fetch the feeds associated with the logged-in user
+        // $userFeeds = $userFeedRepository->findBy(['user' => $user]);
+
         return $this->render('feed/index.html.twig', [
-            'feeds' => $feedRepository->findAll(),
+            'user_feeds' => $userFeedRepository->findBy(['user' => $user]),
         ]);
     }
 
     #[Route('/new', name: 'app_feed_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, FeedRepository $feedRepository): Response
     {
-        $feed = new Feed();
-        $form = $this->createForm(FeedType::class, $feed);
+        $form = $this->createForm(FeedType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($feed);
+            $formData = $form->getData();
+
+            // Get feed data from the form
+            $url = $formData['url'];
+            $name = $formData['name'];
+
+            // Vérifier si un flux avec cette URL existe déjà
+            $feed = $feedRepository->findOneBy(['url' => $url]);
+
+            // Si le flux n'existe pas, le créer
+            if (!$feed) {
+                $feed = new Feed();
+                $feed->setUrl($url);
+                $entityManager->persist($feed);
+                $entityManager->flush(); // Flush pour obtenir l'ID du feed
+            }
+
+            // Créer une association UserFeed
+            $user = $this->getUser(); // L'utilisateur connecté
+
+            if (!$user) {
+                throw $this->createAccessDeniedException('User not logged in');
+            }
+
+            $userFeed = new UserFeed();
+            $userFeed->setUser($user);
+            $userFeed->setFeed($feed);
+            $userFeed->setTitle($name);
+            $userFeed->setCreatedAt(new \DateTimeImmutable());
+            $userFeed->setUpdatedAt(new \DateTime());
+
+            $entityManager->persist($userFeed);
             $entityManager->flush();
 
             return $this->redirectToRoute('app_feed_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('feed/new.html.twig', [
-            'feed' => $feed,
             'form' => $form,
         ]);
     }
@@ -51,15 +91,16 @@ final class FeedController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_feed_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Feed $feed, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Feed $feed, EntityManagerInterface $entityManager, FeedRepository $feedRepository): Response
     {
-        $form = $this->createForm(FeedType::class, $feed);
+        $form = $this->createForm(FeedType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+            $formData = $form->getData();
+            // $entityManager->flush();
 
-            return $this->redirectToRoute('app_feed_index', [], Response::HTTP_SEE_OTHER);
+            // return $this->redirectToRoute('app_feed_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('feed/edit.html.twig', [
@@ -71,7 +112,7 @@ final class FeedController extends AbstractController
     #[Route('/{id}', name: 'app_feed_delete', methods: ['POST'])]
     public function delete(Request $request, Feed $feed, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$feed->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $feed->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($feed);
             $entityManager->flush();
         }
